f82e950a486a786c082557cf3cc3dadaa4e8d3c2
HadleySo <71105018+HadleySo@users.noreply.github.com>
2021-06-07 17:28:49 -0700

    Create cwebp-convert.py

---
+++ cwebp-convert.py
@@ -0,0 +1,14 @@
+import glob
+import os
+
+directory = input("Directory >>> \n")
+quality = input("Quality (0 to 100) >>> \n")
+extentions = ['.png', '.jpeg']
+
+for ext in extentions:
+    for filepath in glob.iglob(directory + '/*' + ext):
+        print(filepath)
+        filename_long = os.path.basename(filepath).split(".")
+        filename = filename_long[len(filename_long) - 2]
+        os.system("cwebp -q " + quality + " " + filepath + " -o " + directory + "/" + filename + ".webp")
+        
