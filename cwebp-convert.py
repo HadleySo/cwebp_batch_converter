a7850a9156859c87f84ce5db142285a3ea76b653
HadleySo <71105018+HadleySo@users.noreply.github.com>
2021-06-07 17:40:28 -0700

    Update cwebp-convert.py

--- cwebp-convert.py
+++ cwebp-convert.py
@@ -3,7 +3,7 @@ import os
 
 directory = input("Directory >>> \n")
 quality = input("Quality (0 to 100) >>> \n")
-extentions = ['.png', '.jpeg', '.jpg']
+extentions = ['.png', '.jpeg', '.jpg', '.JPG']
 
 for ext in extentions:
     for filepath in glob.iglob(directory + '/*' + ext):
